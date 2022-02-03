03a33a584f184297e19a95fe50c942b3a4af853d
WillyWoosley <wdwoosley@gmail.com>
2022-02-02 16:12:04 -0800

    added message/signature verification, modified signing data handling

--- src/lamport_signature.rs
+++ src/lamport_signature.rs
@@ -18,6 +18,7 @@ impl PrivateKey {
     }
 }
 
+#[derive(Debug)]
 pub struct PublicKey {
     pub key_options: Vec<(BigUint, BigUint)>,
 }
@@ -39,11 +40,7 @@ impl PublicKey {
     }
 }
 
-pub struct Signature {
-    pub msg_hash: BigUint,
-    pub sig: Vec<BigUint>,
-}
-
+#[derive(Debug)]
 pub struct KeyPair {
     pub private: PrivateKey,
     pub public: PublicKey,
@@ -57,20 +54,55 @@ impl KeyPair {
         KeyPair {public, private}
     }
 
-    pub fn sign(&self, msg: &str) -> Signature {
+    pub fn sign(self, msg: &str) -> Signature {
         let mut hasher = Sha256::new();
         hasher.update(msg);
         let msg_hash = BigUint::from_bytes_be(&hasher.finalize());
         
         let mut sig = Vec::with_capacity(256); 
-        for i in 0..256 {
+        for (i, (priv0, priv1)) in self.private.key_options.into_iter().enumerate() {
             if msg_hash.bit(i as u64) {
-                sig.push(self.private.key_options[i].1.clone());
+                sig.push(priv1);
             } else {
-                sig.push(self.private.key_options[i].0.clone());
+                sig.push(priv0);
             }
         }
 
-        Signature {msg_hash, sig}
+        Signature {
+            pub_key: self.public,
+            sig,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Signature {
+    pub pub_key: PublicKey,
+    pub sig: Vec<BigUint>,
+}
+
+impl Signature {
+    pub fn verify(&self, msg: &str) -> bool {
+        let mut msg_hasher = Sha256::new();
+        msg_hasher.update(msg);
+        let msg_hash = BigUint::from_bytes_be(&msg_hasher.finalize());
+
+        for (i, (pub0, pub1)) in self.pub_key.key_options.iter().enumerate() {
+            let mut sig_hasher = Sha256::new();
+            sig_hasher.update(self.sig[i].to_bytes_be());
+            let sig_hash = BigUint::from_bytes_be(&sig_hasher.finalize());
+
+            if msg_hash.bit(i as u64) {
+                if &sig_hash != pub1 {
+                    return false;
+                }
+            } else {
+                if &sig_hash != pub0 {
+                    return false;
+                }
+            }
+        }
+
+        true
     }
 }
