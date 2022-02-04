9c85502a6412dd11b95ff28a0111714061a02304
WillyWoosley <wdwoosley@gmail.com>
2022-02-03 18:19:52 -0800

    added generalized sign/verify for all Read trait objects

--- src/lamport_signature.rs
+++ src/lamport_signature.rs
@@ -2,6 +2,9 @@ use sha2::{Sha256, Digest};
 use rand::{thread_rng, Rng};
 use num_bigint::{BigUint, RandomBits};
 
+use std::error::Error;
+use std::io;
+
 #[derive(Debug)]
 pub struct PrivateKey {
     pub key_options: Vec<(BigUint, BigUint)>,
@@ -53,8 +56,28 @@ impl KeyPair {
 
         KeyPair {public, private}
     }
+    
+    pub fn sign(self, msg: &mut dyn io::Read) -> Result<Signature, Box<dyn Error>> {
+        let mut hasher = Sha256::new();
+        let _ = io::copy(msg, &mut hasher)?;
+        let msg_hash = BigUint::from_bytes_be(&hasher.finalize());
 
-    pub fn sign(self, msg: &str) -> Signature {
+        let mut sig = Vec::with_capacity(256); 
+        for (i, (priv0, priv1)) in self.private.key_options.into_iter().enumerate() {
+            if msg_hash.bit(i as u64) {
+                sig.push(priv1);
+            } else {
+                sig.push(priv0);
+            }
+        }
+
+        Ok(Signature {
+            pub_key: self.public,
+            sig,
+        })
+    }
+
+    pub fn sign_string(self, msg: &str) -> Signature {
         let mut hasher = Sha256::new();
         hasher.update(msg);
         let msg_hash = BigUint::from_bytes_be(&hasher.finalize());
@@ -82,11 +105,23 @@ pub struct Signature {
 }
 
 impl Signature {
-    pub fn verify(&self, msg: &str) -> bool {
+    pub fn verify(&self, msg: &mut dyn io::Read) -> Result<bool, Box<dyn Error>> {
+        let mut msg_hasher = Sha256::new();
+        let _ = io::copy(msg, &mut msg_hasher)?;
+        let msg_hash = BigUint::from_bytes_be(&msg_hasher.finalize());
+    
+        Ok(self.check_against_hash(&msg_hash))
+    }
+
+    pub fn verify_string(&self, msg: &str) -> bool {
         let mut msg_hasher = Sha256::new();
         msg_hasher.update(msg);
         let msg_hash = BigUint::from_bytes_be(&msg_hasher.finalize());
-
+        
+        self.check_against_hash(&msg_hash)
+    }
+    
+    fn check_against_hash(&self, msg_hash: &BigUint) -> bool {
         for (i, (pub0, pub1)) in self.pub_key.key_options.iter().enumerate() {
             let mut sig_hasher = Sha256::new();
             sig_hasher.update(self.sig[i].to_bytes_be());
