359c4eafe97045fcd603c6e39b14893728f6aaf7
WillyWoosley <wdwoosley@gmail.com>
2022-02-08 17:18:47 -0800

    substituting hasher for Digest trait object, encountering ownership issues, does not build

--- src/lamport_signature.rs
+++ src/lamport_signature.rs
@@ -1,4 +1,4 @@
-use sha2::{Sha256, Digest};
+use digest::DynDigest;
 use rand::{thread_rng, Rng};
 use num_bigint::{BigUint, RandomBits};
 
@@ -27,40 +27,40 @@ pub struct PublicKey {
 }
 
 impl PublicKey {
-    pub fn generate(private: &PrivateKey) -> Self {
+    pub fn generate(private: &PrivateKey, hasher: &'static dyn DynDigest) -> Self {
         let mut key_options = Vec::with_capacity(256);
 
         for val in &private.key_options {
-            let mut hash0 = Sha256::new();
-            let mut hash1 = Sha256::new();
-            hash0.update(val.0.to_bytes_be());
-            hash1.update(val.1.to_bytes_be());
-            key_options.push((BigUint::from_bytes_be(&hash0.finalize()), 
-                      BigUint::from_bytes_be(&hash1.finalize())));
+            hasher.update(&val.0.to_bytes_be());
+            let hash0 = BigUint::from_bytes_be(&hasher.finalize_reset());
+            hasher.update(&val.1.to_bytes_be());
+            let hash1 = BigUint::from_bytes_be(&hasher.finalize_reset());
+            key_options.push((hash0, hash1));
         }
 
         PublicKey {key_options}
     }
 }
 
-#[derive(Debug)]
 pub struct KeyPair {
     pub private: PrivateKey,
     pub public: PublicKey,
+    pub hasher: &'static dyn DynDigest, 
 }
 
 impl KeyPair {
-    pub fn generate() -> Self {
+    pub fn generate(hasher: &'static dyn DynDigest) -> Self {
         let private = PrivateKey::generate();
-        let public = PublicKey::generate(&private);
+        let public = PublicKey::generate(&private, hasher);
 
-        KeyPair {public, private}
+        KeyPair {public, private, hasher}
     }
     
     pub fn sign(self, msg: &mut dyn io::Read) -> Result<Signature, Box<dyn Error>> {
-        let mut hasher = Sha256::new();
-        let _ = io::copy(msg, &mut hasher)?;
-        let msg_hash = BigUint::from_bytes_be(&hasher.finalize());
+        let mut buffer = Vec::new();
+        io::copy(msg, &mut buffer)?;
+        self.hasher.update(&buffer);
+        let msg_hash = BigUint::from_bytes_be(&self.hasher.finalize_reset());
 
         let mut sig = Vec::with_capacity(256); 
         for (i, (priv0, priv1)) in self.private.key_options.into_iter().enumerate() {
@@ -73,14 +73,14 @@ impl KeyPair {
 
         Ok(Signature {
             pub_key: self.public,
+            hasher: self.hasher,
             sig,
         })
     }
 
     pub fn sign_string(self, msg: &str) -> Signature {
-        let mut hasher = Sha256::new();
-        hasher.update(msg);
-        let msg_hash = BigUint::from_bytes_be(&hasher.finalize());
+        self.hasher.update(msg.as_bytes());
+        let msg_hash = BigUint::from_bytes_be(&self.hasher.finalize_reset());
         
         let mut sig = Vec::with_capacity(256); 
         for (i, (priv0, priv1)) in self.private.key_options.into_iter().enumerate() {
@@ -93,39 +93,39 @@ impl KeyPair {
 
         Signature {
             pub_key: self.public,
+            hasher: self.hasher,
             sig,
         }
     }
 }
 
-#[derive(Debug)]
 pub struct Signature {
     pub pub_key: PublicKey,
     pub sig: Vec<BigUint>,
+    pub hasher: &'static dyn DynDigest,
 }
 
 impl Signature {
     pub fn verify(&self, msg: &mut dyn io::Read) -> Result<bool, Box<dyn Error>> {
-        let mut msg_hasher = Sha256::new();
-        let _ = io::copy(msg, &mut msg_hasher)?;
-        let msg_hash = BigUint::from_bytes_be(&msg_hasher.finalize());
+        let mut buffer = Vec::new();
+        io::copy(msg, &mut buffer)?;
+        self.hasher.update(&buffer);
+        let msg_hash = BigUint::from_bytes_be(&self.hasher.finalize_reset());
     
         Ok(self.check_against_hash(&msg_hash))
     }
 
     pub fn verify_string(&self, msg: &str) -> bool {
-        let mut msg_hasher = Sha256::new();
-        msg_hasher.update(msg);
-        let msg_hash = BigUint::from_bytes_be(&msg_hasher.finalize());
+        self.hasher.update(msg.as_bytes());
+        let msg_hash = BigUint::from_bytes_be(&self.hasher.finalize_reset());
         
         self.check_against_hash(&msg_hash)
     }
     
     fn check_against_hash(&self, msg_hash: &BigUint) -> bool {
         for (i, (pub0, pub1)) in self.pub_key.key_options.iter().enumerate() {
-            let mut sig_hasher = Sha256::new();
-            sig_hasher.update(self.sig[i].to_bytes_be());
-            let sig_hash = BigUint::from_bytes_be(&sig_hasher.finalize());
+            self.hasher.update(&self.sig[i].to_bytes_be());
+            let sig_hash = BigUint::from_bytes_be(&self.hasher.finalize_reset());
 
             if msg_hash.bit(i as u64) {
                 if &sig_hash != pub1 {
